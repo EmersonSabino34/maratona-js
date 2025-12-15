2deb9ed6765dad6b271ebba29be363d883c5aa68
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2025-12-15 19:43:37 +0000

    Update funcao-01.js

--- funcao-01.js
+++ funcao-01.js
@@ -2,3 +2,29 @@
 (function somar(a=10, b=5){
   console.log(a+b);
 })();
+
+function verificarIdade(idade) {
+  return idade >= 18 ? "Maior de idade" : "Menor de idade";
+}
+
+const celsiusParaFahrenheit = (c) => (c * 9/5) + 32;
+
+function parOuImpar(numero) {
+  return numero % 2 === 0 ? "Par" : "Ímpar";
+}
+const contarLetras = (texto) => texto.length;
+
+
+function media(n1, n2, n3) {
+  return (n1 + n2 + n3) / 3;
+}
+
+function somarArray(numeros) {
+  let total = 0;
+
+  for (let num of numeros) {
+    total += num;
+  }
+
+  return total;
+}
